6866e70233c3c5a8b125d90c3c0dc24a454ee886
Chidubem <nwekedubemph@gmail.com>
2023-09-01 02:45:45 -0700

    first commit

--- tasks/task_one.js
+++ tasks/task_one.js
@@ -1,2 +1,70 @@
+const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+let catArray =[];
+let currentPage = 1;
+let itemsPerPage = 3;
 
-// Write something
+let prevBtn = document.getElementById('previous');
+let nextBtn = document.getElementById('next');
+
+
+async function getApi(){
+    const response = await fetch(url);
+    const data = await response.json();
+    catArray = data.categories
+}
+
+async function showPage(){
+    await getApi()
+
+
+    let startIndex= (currentPage-1) *itemsPerPage;
+    let endIndex= startIndex + itemsPerPage;
+
+    let itemsToShow = catArray.slice(startIndex,endIndex)
+
+    let display = document.getElementById('menu');
+    display.innerHTML = "";
+    
+    itemsToShow.forEach(item => {
+        let numTag = document.createElement('p');
+        let imgTag = document.createElement('img');
+        let headerTag = document.createElement('h3');
+        let descTag = document.createElement('p');
+        
+        numTag.innerText = item.idCategory;
+        imgTag.src = item.strCategoryThumb;
+        headerTag.innerText = item.strCategory;
+        descTag.innerText = item.strCategoryDescription;
+        
+        display.appendChild(numTag);
+        display.appendChild(headerTag);
+        display.appendChild(imgTag);
+        display.appendChild(descTag);
+    }); 
+    hideBtns()
+}
+showPage()
+prevBtn.addEventListener('click',()=>{
+    currentPage--
+    showPage()
+});
+nextBtn.addEventListener('click', () =>{
+    currentPage++
+    showPage()
+})
+
+function hideBtns(){
+    if (currentPage === 1){
+        prevBtn.style.display = 'none';
+    }else{
+        prevBtn.style.display = 'initial'
+    };
+
+    let pageLimit =Math.ceil( catArray.length / itemsPerPage);
+    if(currentPage === pageLimit){
+        nextBtn.style.display = 'none';
+    }else{
+        nextBtn.style.display = 'initial'
+    }
+    
+}
