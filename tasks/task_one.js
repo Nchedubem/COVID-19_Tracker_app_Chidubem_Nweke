15b58ac9f9ac0c61d9644ce5e2b4ae150eb1607b
Chidubem <nwekedubemph@gmail.com>
2023-09-01 03:40:18 -0700

    second commit

--- tasks/task_one.js
+++ tasks/task_one.js
@@ -1,70 +1,77 @@
 const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
-let catArray =[];
+let catArray = [];
 let currentPage = 1;
 let itemsPerPage = 3;
 
 let prevBtn = document.getElementById('previous');
 let nextBtn = document.getElementById('next');
 
-
-async function getApi(){
-    const response = await fetch(url);
-    const data = await response.json();
-    catArray = data.categories
+async function getApi() {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        catArray = data.categories;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-async function showPage(){
-    await getApi()
+async function showPage() {
+    await getApi();
 
+    let startIndex = (currentPage - 1) * itemsPerPage;
+    let endIndex = startIndex + itemsPerPage;
 
-    let startIndex= (currentPage-1) *itemsPerPage;
-    let endIndex= startIndex + itemsPerPage;
-
-    let itemsToShow = catArray.slice(startIndex,endIndex)
+    let itemsToShow = catArray.slice(startIndex, endIndex);
 
     let display = document.getElementById('menu');
     display.innerHTML = "";
-    
+
     itemsToShow.forEach(item => {
         let numTag = document.createElement('p');
         let imgTag = document.createElement('img');
         let headerTag = document.createElement('h3');
         let descTag = document.createElement('p');
-        
+
         numTag.innerText = item.idCategory;
         imgTag.src = item.strCategoryThumb;
         headerTag.innerText = item.strCategory;
         descTag.innerText = item.strCategoryDescription;
-        
+
         display.appendChild(numTag);
         display.appendChild(headerTag);
         display.appendChild(imgTag);
         display.appendChild(descTag);
-    }); 
-    hideBtns()
+    });
+    hideBtns();
 }
-showPage()
-prevBtn.addEventListener('click',()=>{
-    currentPage--
-    showPage()
+
+showPage();
+
+prevBtn.addEventListener('click', () => {
+    currentPage--;
+    showPage();
 });
-nextBtn.addEventListener('click', () =>{
-    currentPage++
-    showPage()
-})
 
-function hideBtns(){
-    if (currentPage === 1){
+nextBtn.addEventListener('click', () => {
+    currentPage++;
+    showPage();
+});
+
+function hideBtns() {
+    if (currentPage === 1) {
         prevBtn.style.display = 'none';
-    }else{
-        prevBtn.style.display = 'initial'
-    };
-
-    let pageLimit =Math.ceil( catArray.length / itemsPerPage);
-    if(currentPage === pageLimit){
-        nextBtn.style.display = 'none';
-    }else{
-        nextBtn.style.display = 'initial'
+    } else {
+        prevBtn.style.display = 'initial';
     }
-    
+
+    let pageLimit = Math.ceil(catArray.length / itemsPerPage);
+    if (currentPage === pageLimit) {
+        nextBtn.style.display = 'none';
+    } else {
+        nextBtn.style.display = 'initial';
+    }
 }
