99bdead47a1b0d362034b00ed5ae80918827522d
Chidubem <nwekedubemph@gmail.com>
2023-09-01 04:24:22 -0700

    second commit

--- tasks/task_one.js
+++ tasks/task_one.js
@@ -1,77 +1,87 @@
-const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
-let catArray = [];
-let currentPage = 1;
-let itemsPerPage = 3;
-
-let prevBtn = document.getElementById('previous');
-let nextBtn = document.getElementById('next');
-
-async function getApi() {
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        catArray = data.categories;
-    } catch (error) {
-        console.error(error);
+document.addEventListener('DOMContentLoaded', () => {
+    const api = 'https://disease.sh/v3/covid-19/countries';
+    const container = document.getElementById('genCon');
+  
+    async function fetchCovidData(api) {
+      try {
+        const response = await fetch(api);
+        return await response.json();
+      } catch (error) {
+        console.error('Error fetching COVID data:', error);
+        return [];
+      }
     }
-}
-
-async function showPage() {
-    await getApi();
-
-    let startIndex = (currentPage - 1) * itemsPerPage;
-    let endIndex = startIndex + itemsPerPage;
-
-    let itemsToShow = catArray.slice(startIndex, endIndex);
-
-    let display = document.getElementById('menu');
-    display.innerHTML = "";
-
-    itemsToShow.forEach(item => {
-        let numTag = document.createElement('p');
-        let imgTag = document.createElement('img');
-        let headerTag = document.createElement('h3');
-        let descTag = document.createElement('p');
-
-        numTag.innerText = item.idCategory;
-        imgTag.src = item.strCategoryThumb;
-        headerTag.innerText = item.strCategory;
-        descTag.innerText = item.strCategoryDescription;
-
-        display.appendChild(numTag);
-        display.appendChild(headerTag);
-        display.appendChild(imgTag);
-        display.appendChild(descTag);
-    });
-    hideBtns();
-}
-
-showPage();
-
-prevBtn.addEventListener('click', () => {
-    currentPage--;
-    showPage();
-});
-
-nextBtn.addEventListener('click', () => {
-    currentPage++;
-    showPage();
-});
-
-function hideBtns() {
-    if (currentPage === 1) {
-        prevBtn.style.display = 'none';
-    } else {
-        prevBtn.style.display = 'initial';
+  
+    function createCard(country) {
+      const card = document.createElement('div');
+      card.className = 'card';
+  
+      const flag = document.createElement('img');
+      flag.className = 'flag';
+      flag.src = country.countryInfo.flag;
+  
+      const name = document.createElement('h2');
+      name.textContent = `Country: ${country.country}`;
+  
+      const continent = document.createElement('h3');
+      continent.textContent = `Continent: ${country.continent}`;
+  
+      const overlay = createOverlay(country);
+  
+      flag.addEventListener('click', () => {
+        overlay.style.display = 'block';
+      });
+  
+      card.append(flag, name, continent, overlay);
+      return card;
     }
-
-    let pageLimit = Math.ceil(catArray.length / itemsPerPage);
-    if (currentPage === pageLimit) {
-        nextBtn.style.display = 'none';
-    } else {
-        nextBtn.style.display = 'initial';
+  
+    function createOverlay(country) {
+      const overlay = document.createElement('div');
+      overlay.className = 'overlay';
+  
+      const overCon = document.createElement('div');
+      overCon.className = 'overCon';
+  
+      const country2 = createParagraph(`Country: ${country.country}`);
+      const countryPop = createParagraph(`Population: ${country.population}`);
+      const reportedToday = createParagraph(`Reported cases today: ${country.todayCases}`);
+      const totalReported = createParagraph(`Total reported cases: ${country.cases}`);
+      const deathCount = createParagraph(`Total deaths: ${country.deaths}`);
+      const recovered = createParagraph(`Total recovered: ${country.recovered}`);
+  
+      const btn = document.createElement('button');
+      btn.textContent = 'X';
+      btn.className = 'closeBtn';
+  
+      btn.addEventListener('click', () => {
+        overlay.style.display = 'none';
+      });
+  
+      overCon.append(country2, countryPop, reportedToday, totalReported, deathCount, recovered, btn);
+      overlay.appendChild(overCon);
+  
+      return overlay;
     }
-}
+  
+    function createParagraph(text) {
+      const paragraph = document.createElement('p');
+      paragraph.textContent = text;
+      return paragraph;
+    }
+  
+    async function getCovidData() {
+      try {
+        const data = await fetchCovidData(api);
+  
+        data.forEach((country) => {
+          const card = createCard(country);
+          container.append(card);
+        });
+      } catch (error) {
+        console.error('Error getting COVID data:', error);
+      }
+    }
+  
+    getCovidData();
+  });
